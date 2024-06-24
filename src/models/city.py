15b6d07f51929fcfa5b0780a2182d9027851e28e
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-24 14:56:39 +0200

    pulled from johan

--- src/models/city.py
+++ src/models/city.py
@@ -1,10 +1,15 @@
 from src.models.base import Base
 from src.models.country import Country
+from src import db
 
 
-class City(Base):
-    name: str
-    country_code: str
+class City(Base, db.Model):
+    __tablename__ = 'cities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    country_code = db.Column(db.String(2), db.ForeignKey('countries.code'), nullable=False)
+    country = db.relationship('Country', backref=db.backref('cities', lazy=True))
 
     def __init__(self, name: str, country_code: str, **kw) -> None:
         super().__init__(**kw)
--- src/models/place.py
+++ src/models/place.py
@@ -1,24 +1,27 @@
 from src.models.base import Base
 from src.models.city import City
 from src.models.user import User
+from src import db
 
 
-class Place(Base):
+class Place(Base, db.Model):
     """Place:
     amenity_ids (List of UUIDs referencing Amenities),
     """
+    __tablename__ = 'places'
 
-    name: str
-    description: str
-    address: str
-    latitude: float
-    longitude: float
-    host_id: str
-    city_id: str
-    price_per_night: int
-    number_of_rooms: int
-    number_of_bathrooms: int
-    max_guests: int
+    id = db.Column(db.String(36), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    address = db.Column(db.String(255), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    host_id = db.Column(db.String(36), db.ForeignKey('hosts.id'), nullable=False)
+    city_id = db.Column(db.String(36), db.ForeignKey('cities.id'), nullable=False)
+    price_per_night = db.Column(db.Integer, nullable=False)
+    number_of_rooms = db.Column(db.Integer, nullable=False)
+    number_of_bathrooms = db.Column(db.Integer, nullable=False)
+    max_guests = db.Column(db.Integer, nullable=False)
 
     def __init__(self, data: dict | None = None, **kw) -> None:
         super().__init__(**kw)
