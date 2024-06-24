9e2b18e8929df10756b470e7d25ae4d046764704
Yousra <142456388+yousrakdc@users.noreply.github.com>
2024-06-24 10:28:41 +0200

    Create user.py

---
+++ src/models/user.py
@@ -0,0 +1,62 @@
+from src.models.base import Base
+
+
+class User(Base):
+    email: str
+    first_name: str
+    last_name: str
+
+    def __init__(self, email: str, first_name: str, last_name: str, **kw) -> None:
+        super().__init__(**kw)
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self) -> str:
+        return f"<User {self.id} ({self.email})>"
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+        }
+
+    @staticmethod
+    def create(user: dict) -> "User":
+        from src.persistence import db
+
+        users: list["User"] = User.get_all()
+
+        for u in users:
+            if u.email == user["email"]:
+                raise ValueError("User already exists")
+
+        new_user = User(**user)
+
+        db.save(new_user)
+
+        return new_user
+
+    @staticmethod
+    def update(user_id: str, data: dict) -> "User | None":
+        from src.persistence import db
+
+        user: User | None = User.get(user_id)
+
+        if not user:
+            return None
+
+        if "email" in data:
+            user.email = data["email"]
+        if "first_name" in data:
+            user.first_name = data["first_name"]
+        if "last_name" in data:
+            user.last_name = data["last_name"]
+
+        db.update(user)
+
+        return user
