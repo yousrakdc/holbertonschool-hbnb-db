dfee2c24344cd3fa4d1f72424841aeaffffcd067
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-06-26 15:31:33 +0200

    Update country.py

--- src/models/country.py
+++ src/models/country.py
@@ -3,7 +3,6 @@ from src import db
 class Country(db.Model):
     __tablename__ = 'countries'
 
-    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     code = db.Column(db.String(2), nullable=False, unique=True)
 
