e8af4e54598b85c82c92c06c5b3ef788130e4753
Prof-Ursi <8743@holbertonstudents.com>
2024-06-26 10:57:11 +0200

    task 1.3 adding SQLite database check in Development config mode, and erasing testing mode (can be added back, to be tested)

--- src/config.py
+++ src/config.py
@@ -1,29 +1,69 @@
 import os
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
-
+import sqlite3
+#Part of the the Flask_SQLAlchemy configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     DEBUG = False
     TESTING = False
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret')
+
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///hbnb_dev.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
+
+# Development configuration inherits from Config
 class DevelopmentConfig(Config):
-    DEBUG = True
+    DEBUG = True # Enable debug mode for development
+    TESTING = True # Enable testing mode for development
+
+    # Development-specific database URI, defaults to SQLite if not set
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///development.db")
+
+    #Path to the default SQLite database file.
+    db_file = 'holbertonschool-hbnb-db/src/persistence/SQLite_database.db'
+    #(temporary name, to be updated when definitive SQLite database created)
+
+    # Check if the database exists, and if not creates it.
+    if not os.path.exists(db_file):
+        conn = sqlite3.connect(db_file)
+        cursor = conn.cursor()
+        cursor.execute
+        (
+        '''
+        CREATE TABLE IF NOT EXISTS users 
+            (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            email TEXT NOT NULL UNIQUE
+            )
+        '''
+        )
+        print("Database created successfully!")
+        conn.commit()
+        conn.close()
+    else:
+        print("SQLite database already exists.")
 
 
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-
-
+# Production configuration inherits from Config
 class ProductionConfig(Config):
+    # Production database URI, defaults to PostgreSQL if DATABASE_URL is set, otherwise local SQLite
     DEBUG = False
+    TESTING = False 
+
+    # Initialize PostgreSQL database when 
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "postgresql://user:password@localhost/hbnb_prod"
+        "DATABASE_URL",
+        "postgresql://user:password@localhost/hbnb_prod"
     )
 
 app = Flask(__name__)
