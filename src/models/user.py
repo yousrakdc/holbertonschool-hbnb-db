dc089b84f37786a5cd45afc46efc5f99578358e0
yousrakdc <yousrakdc@gmail.com>
2024-06-26 15:17:16 +0200

    password and admin added

--- src/models/user.py
+++ src/models/user.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, String, DateTime, Boolean
 from sqlalchemy.sql import func
 from datetime import datetime
-from src import db
-
+from src import db, bcrypt
 
 class User(Base):
     
@@ -18,11 +17,13 @@ class User(Base):
     updated_at = db.Column(db.DateTime, onupdate=db.func.current_timestamp())
 
 
-    def __init__(self, email: str, first_name: str, last_name: str, **kw) -> None:
+    def __init__(self, email: str, first_name: str, last_name: str, password: str, is_admin: bool = False, **kw):
         super().__init__(**kw)
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
+        self.is_admin = is_admin
+        self.set_password(password)
 
     def __repr__(self) -> str:
         return f"<User {self.id} ({self.email})>"
@@ -33,10 +34,19 @@ class User(Base):
             "email": self.email,
             "first_name": self.first_name,
             "last_name": self.last_name,
+            "is_admin": self.is_admin,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat(),
         }
 
+    def set_password(self, password: str):
+        """Set password method"""
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password: str) -> bool:
+        """Check password method"""
+        return bcrypt.check_password_hash(self.password_hash, password)
+    
     @staticmethod
     def create(user: dict) -> "User":
         from src.persistence import db
