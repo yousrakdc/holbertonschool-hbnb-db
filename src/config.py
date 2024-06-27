e57f71017fb641119e55e62fb51b4f7aa2aadd9a
yousrakdc <yousrakdc@gmail.com>
2024-06-27 11:52:56 +0200

    config

--- src/config.py
+++ src/config.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '../.env'))
 
@@ -11,41 +12,19 @@ class Config:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'hohohoitsasecret')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'hohohoitsasecret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///hbnb_dev.db')
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
+    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///hbnb_dev.db')
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///hbnb_dev.db')
-    
-    # Example for database creation logic
-    db_file = os.path.join(basedir, 'SQLite_database.db')
-    try:
-        if not os.path.exists(db_file):
-            conn = sqlite3.connect(db_file)
-            cursor = conn.cursor()
+    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///development.db')
 
-            # Create 'users' table if it does not exist
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS users (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    username TEXT NOT NULL UNIQUE,
-                    email TEXT NOT NULL UNIQUE
-                )
-            ''')
-
-            print("Database created successfully!")
-
-            conn.commit()
-    except sqlite3.Error as e:
-        print(f"SQLite error: {e}")
-    finally:
-        if 'conn' in locals():
-            conn.close()
+class ProductionConfig(Config):
+    DEBUG = False
+    DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/hbnb_prod')
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL', 'sqlite:///:memory:')
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL', 'postgresql://user:password@localhost/hbnb_prod')
+    DATABASE_URL = 'sqlite:///:memory:'
