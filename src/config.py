dc7720192524dcc8b5d80de49c6b12831215c10e
SamJouini <samantha.u.h.jouini@gmail.com>
2024-06-28 12:50:17 +0200

    Database de donnée

--- src/config.py
+++ src/config.py
@@ -1,24 +1,30 @@
 import os
+import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '../.env'))
 
 class Config:
     DEBUG = False
     TESTING = False
-
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///hbnb_dev.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SECRET_KEY = os.getenv('SECRET_KEY', 'hohohoitsasecret')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'hohohoitsasecret')
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
+    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite://SQL/hbnb.db')
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///development.db')
 
+class ProductionConfig(Config):
+    DEBUG = False
+    DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/hbnb_prod')
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-
-
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "postgresql://user:password@localhost/hbnb_prod"
-    )
+    DATABASE_URL = 'sqlite:///:memory:'
