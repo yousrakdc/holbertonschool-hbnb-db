585c4a121c69d958d4c9c9806ea0e1a4841b7eaa
yousrakdc <yousrakdc@gmail.com>
2024-07-02 10:59:24 +0200

    tested

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,52 +1,57 @@
-import os
 import unittest
-from dotenv import load_dotenv
-
-# Assuming the config file is named config.py and resides in the same directory
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from src.config import Config, DevelopmentConfig, ProductionConfig, TestingConfig
 
-
 class TestConfig(unittest.TestCase):
-    def setUp(self):
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        load_dotenv(os.path.join(basedir, '../.env'))
 
-    def test_default_config(self):
-        # Unset DATABASE_URL to test default value
-        if 'DATABASE_URL' in os.environ:
-            del os.environ['DATABASE_URL']
-        config = Config()
-        self.assertFalse(config.DEBUG)
-        self.assertFalse(config.TESTING)
-        self.assertFalse(config.SQLALCHEMY_TRACK_MODIFICATIONS)
-        self.assertEqual(config.SECRET_KEY, os.getenv('SECRET_KEY', 'hohohoitsasecret'))
-        self.assertEqual(config.JWT_SECRET_KEY, os.getenv('JWT_SECRET_KEY', 'hohohoitsasecret'))
-        self.assertEqual(config.JWT_ACCESS_TOKEN_EXPIRES, 3600)
-        self.assertEqual(config.DATABASE_URL, 'sqlite:///hbnb_dev.db')
+    def test_config_default(self):
+        app = Flask(__name__)
+        app.config.from_object(Config)
 
-    def test_development_config(self):
-        # Unset DATABASE_URL to test default value
-        if 'DATABASE_URL' in os.environ:
-            del os.environ['DATABASE_URL']
-        config = DevelopmentConfig()
-        self.assertTrue(config.DEBUG)
-        self.assertEqual(config.DATABASE_URL, 'sqlite:///development.db')
+        self.assertFalse(app.config['DEBUG'])
+        self.assertFalse(app.config['TESTING'])
+        self.assertFalse(app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
+        self.assertEqual(app.config['SECRET_KEY'], 'hohohoitsasecret')
+        self.assertEqual(app.config['JWT_SECRET_KEY'], 'hohohoitsasecret')
+        self.assertEqual(app.config['JWT_ACCESS_TOKEN_EXPIRES'], 3600)
+        self.assertEqual(app.config['SQLALCHEMY_DATABASE_URI'], 'sqlite:///hbnb.db')
 
-    def test_production_config(self):
-        # Unset DATABASE_URL to test default value
-        if 'DATABASE_URL' in os.environ:
-            del os.environ['DATABASE_URL']
-        config = ProductionConfig()
-        self.assertFalse(config.DEBUG)
-        self.assertEqual(config.DATABASE_URL, 'postgresql://user:password@localhost/hbnb_prod')
+    def test_config_development(self):
+        app = Flask(__name__)
+        app.config.from_object(DevelopmentConfig)
 
-    def test_testing_config(self):
-        # Unset DATABASE_URL to test default value
-        if 'DATABASE_URL' in os.environ:
-            del os.environ['DATABASE_URL']
-        config = TestingConfig()
-        self.assertTrue(config.TESTING)
-        self.assertEqual(config.DATABASE_URL, 'sqlite:///:memory:')
+        self.assertTrue(app.config['DEBUG'])
+        self.assertFalse(app.config['TESTING'])
+        self.assertFalse(app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
+        self.assertEqual(app.config['SECRET_KEY'], 'hohohoitsasecret')
+        self.assertEqual(app.config['JWT_SECRET_KEY'], 'hohohoitsasecret')
+        self.assertEqual(app.config['JWT_ACCESS_TOKEN_EXPIRES'], 3600)
+        self.assertEqual(app.config['SQLALCHEMY_DATABASE_URI'], 'sqlite:///development.db')
+
+    def test_config_production(self):
+        app = Flask(__name__)
+        app.config.from_object(ProductionConfig)
+
+        self.assertFalse(app.config['DEBUG'])
+        self.assertFalse(app.config['TESTING'])
+        self.assertFalse(app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
+        self.assertEqual(app.config['SECRET_KEY'], 'hohohoitsasecret')
+        self.assertEqual(app.config['JWT_SECRET_KEY'], 'hohohoitsasecret')
+        self.assertEqual(app.config['JWT_ACCESS_TOKEN_EXPIRES'], 3600)
+        self.assertEqual(app.config['SQLALCHEMY_DATABASE_URI'], 'postgresql://user:password@localhost/hbnb_prod')
+
+    def test_config_testing(self):
+        app = Flask(__name__)
+        app.config.from_object(TestingConfig)
+
+        self.assertFalse(app.config['DEBUG'])
+        self.assertTrue(app.config['TESTING'])
+        self.assertFalse(app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
+        self.assertEqual(app.config['SECRET_KEY'], 'hohohoitsasecret')
+        self.assertEqual(app.config['JWT_SECRET_KEY'], 'hohohoitsasecret')
+        self.assertEqual(app.config['JWT_ACCESS_TOKEN_EXPIRES'], 3600)
+        self.assertEqual(app.config['SQLALCHEMY_DATABASE_URI'], 'sqlite:///:memory:')
 
 if __name__ == '__main__':
     unittest.main()
